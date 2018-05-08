8752bd2ea766ff7eada8ac54589ea3c1910413cf
minhtuhcmus <37372491+minhtuhcmus@users.noreply.github.com>
2018-05-08 07:36:08 +0700

    Update AdapterInnerRecyclerView.java

--- app/src/main/java/com/example/dungit/gallery/presentation/uis/adapters/AdapterInnerRecyclerView.java
+++ app/src/main/java/com/example/dungit/gallery/presentation/uis/adapters/AdapterInnerRecyclerView.java
@@ -98,6 +98,7 @@ public class AdapterInnerRecyclerView
                     imgPostion = position;
                 }
                 intent.putExtra(PreviewPhotoActivity.IMG_POSITION, imgPostion);
+                intent.putExtra("filePath",data.get(position).getFile().toString());
                 context.startActivity(intent);
             }
 
