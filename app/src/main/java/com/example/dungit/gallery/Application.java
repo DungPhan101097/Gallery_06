3c938c6728552815e0edf5aaae655c131751721e
minhtuhcmus <37372491+minhtuhcmus@users.noreply.github.com>
2018-05-08 07:32:32 +0700

    Update Application.java

--- app/src/main/java/com/example/dungit/gallery/Application.java
+++ app/src/main/java/com/example/dungit/gallery/Application.java
@@ -1,4 +1,12 @@
 package com.example.dungit.gallery;
 
-class Application {
+import ly.img.android.PESDK;
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        PESDK.init(this,"LICENSE");
+    }
 }
